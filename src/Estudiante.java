f8ee8816ba7f04516a61dd0b4853c3f818e189f9
sebastian <xsebastianzg@gmail.com>
2024-05-19 23:24:57 -0500

    Herencia - Implementación de Constructores por [Nombre]

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -2,6 +2,14 @@ public class Estudiante extends Persona {
     // Propiedad adicional para el grado del estudiante
     private String grado;
 
+    // Constructor para inicializar las propiedades heredadas y propias
+    public Estudiante(String nombre, int edad, String fechaNacimiento, String grado) {
+        establecerNombre(nombre);
+        establecerEdad(edad);
+        establecerFechaNacimiento(fechaNacimiento);
+        this.grado = grado;
+    }
+
     // Sobrescritura del método toString()
     @Override
     public String toString() {
