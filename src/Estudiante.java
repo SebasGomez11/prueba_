f95e141e1e94a5f6d03abd3719dcd913141f362c
sebastian <xsebastianzg@gmail.com>
2024-05-19 23:24:26 -0500

    Herencia - Sobrescritura del método toString() por sebastian

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,4 +1,15 @@
 public class Estudiante extends Persona {
     // Propiedad adicional para el grado del estudiante
     private String grado;
+
+    // Sobrescritura del método toString()
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "nombre='" + obtenerNombre() + '\'' +
+                ", edad=" + obtenerEdad() +
+                ", fechaNacimiento='" + obtenerFechaNacimiento() + '\'' +
+                ", grado='" + grado + '\'' +
+                '}';
+    }
 }
