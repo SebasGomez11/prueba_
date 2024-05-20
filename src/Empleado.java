1da8050b25e5e341b46ac645d65199e7b6418798
sebastian <xsebastianzg@gmail.com>
2024-05-20 00:12:06 -0500

    Herencia y Polimorfismo - Implementación de Clase Empleado por sebastian

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,16 +1,2 @@
-public class Empleado implements Trabajador {
-    @Override
-    public void realizarTarea() {
-        System.out.println("Realizando tarea laboral");
-        // Usando una clase anónima
-        Trabajador trabajadorAnonimo = new Trabajador() {
-            @Override
-            public void realizarTarea() {
-                System.out.println("Realizando tarea temporal");
-            }
-        };
-
-// Usando una lambda
-        Trabajador trabajadorLambda = () -> System.out.println("Realizando tarea temporal");
-    }
+package PACKAGE_NAME;public class Empleado {
 }
